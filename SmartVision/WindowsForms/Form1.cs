631afa33ae28a2d3201c838e5a628cd2daac1142
tomas-drasutis <drasutis.tomas@gmail.com>
2018-09-16 16:53:13 +0300

    A basic GUI. Version 1.0.0.0

--- SmartVision/WindowsForms/Form1.cs
+++ SmartVision/WindowsForms/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -7,13 +8,14 @@ namespace WindowsForms
 {
     public partial class Form1 : Form
     {
+
         private VideoCapture capture; // Takes video from camera as image frames
 
         public Form1()
         {
             InitializeComponent();
-
             EnableWebcam();
+            //homePanel.BringToFront();
         }
 
         /// <summary>
@@ -44,7 +46,7 @@ namespace WindowsForms
             Mat m = new Mat();
             capture.Retrieve(m);
 
-            pictureBox.Image = m.ToImage<Bgr, Byte>().Bitmap; // Sends the frame to the picture box in the GUI
+            scanPictureBox.Image = m.ToImage<Bgr, Byte>().Bitmap; // Sends the frame to the picture box in the GUI
         }
 
         /// <summary>
@@ -53,6 +55,7 @@ namespace WindowsForms
         /// </summary>
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+
             try
             {
                 capture.Dispose();
@@ -61,6 +64,83 @@ namespace WindowsForms
             {
                 Console.WriteLine(exception);
                 throw;
+            }
+        }
+
+        /// <summary>
+        /// Main buttons - switching through panels, exiting application
+        /// Author: Tomas Drasutis
+        /// </summary>
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            homePanel.BringToFront();
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            scanPanel.BringToFront();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            homePanel.BringToFront();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addPersonButton_Click(object sender, EventArgs e)
+        {
+            addPersonPanel.BringToFront();
+        }
+
+        /// <summary>
+        /// Gets information about the missing person
+        /// Author: Tomas Drasutis
+        /// </summary>
+        private void confirmPersonButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string firstName = firstNameBox.Text;
+                string lastName = lastNameBox.Text;
+                string dateOfBirth = dateOfBirthPicker.Value.ToShortDateString();
+                string additionalInformation = adInfoBox.Text;
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+        }
+
+        /// <summary>
+        /// Uploads the picture
+        /// Author: Tomas Drasutis
+        /// </summary>
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            String imageLocation = "";
+
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    personPictureBox.ImageLocation = imageLocation;
+                    personPictureBox.BringToFront();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
             }
         }
     }
