b00db96e034570e4520010356ca5031f84e819e5
JonasChai <jonas.chairutdinovas@gmail.com>
2018-09-16 16:06:48 +0300

    Add MissingPerson

---
+++ SmartVision/WindowsForms/MissingPerson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms
+{
+    class MissingPerson
+    {
+        string name;
+        string surname;
+        string description;
+        DateTime birthday;
+        Image photo;
+
+        public MissingPerson(string name, string surname, string description, DateTime birthday, Image photo)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.description = description;
+            this.birthday = birthday;
+            this.photo = photo;
+        }
+
+    }
+}
