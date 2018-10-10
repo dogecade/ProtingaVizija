3bd258d60f1120ad09200779808f59e8731e4dc5
Arnas Danaitis <arnas.danaitis@gmail.com>
2018-10-10 17:46:36 +0300

    readding placeholder keys file

---
+++ SmartVision/FaceAnalysis/Keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceAnalysis
+{
+    class Keys
+    {
+        public const string apiKey = "key";
+        public const string apiSecret = "secret";
+    }
+}
