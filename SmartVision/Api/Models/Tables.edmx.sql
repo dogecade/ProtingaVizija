8edbaa52dd28f5672e9d359e5a206d56ded94e74
Tomas <vilcinskastomas911@gmail.com>
2018-10-15 20:56:59 +0300

    commit

---
+++ SmartVision/Api/Models/Tables.edmx.sql
@@ -0,0 +1,78 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 10/11/2018 08:22:40
+-- Generated from EDMX file: C:\Users\tomas\Source\Repos\ProtingaVizija\SmartVision\Api\Models\Tables.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [pstop2018];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Table]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Table];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'MissingPersons'
+CREATE TABLE [dbo].[MissingPersons] (
+    [Id] int  NOT NULL,
+    [faceToken] varchar(max)  NOT NULL,
+    [firstName] varchar(max)  NULL,
+    [lastName] varchar(max)  NULL,
+    [lastSeenDate] nvarchar(max)  NOT NULL,
+    [lastSeenLocation] nvarchar(max)  NOT NULL,
+    [Additional_Information] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'ContactPersons'
+CREATE TABLE [dbo].[ContactPersons] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [missingPersonId] nvarchar(max)  NOT NULL,
+    [firstName] nvarchar(max)  NOT NULL,
+    [lastName] nvarchar(max)  NOT NULL,
+    [phoneNumber] nvarchar(max)  NOT NULL,
+    [emailAddress] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'MissingPersons'
+ALTER TABLE [dbo].[MissingPersons]
+ADD CONSTRAINT [PK_MissingPersons]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'ContactPersons'
+ALTER TABLE [dbo].[ContactPersons]
+ADD CONSTRAINT [PK_ContactPersons]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
