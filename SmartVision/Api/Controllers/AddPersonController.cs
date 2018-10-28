10c73322564fa14dbd06e0761fbd825d8766f39e
tomas-drasutis <drasutis.tomas@gmail.com>
2018-10-28 15:21:35 +0200

    Developed the forms, still need improvement.

---
+++ SmartVision/Api/Controllers/AddPersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Api.Controllers
+{
+    public class AddPersonController : Controller
+    {
+        // GET: AddPerson
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MissingPersonView()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartVision/Api/Controllers/CurrentlyMissingController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace Api.Controllers
+{
+    public class CurrentlyMissingController : Controller
+    {
+        // GET: CurrentlyMissing
+        public ActionResult Index()
+        {
+            var missingPeople = new MissingPersonsController().GetMissingPersons().ToList();
+
+            return View(missingPeople);
+        }
+    }
+}
