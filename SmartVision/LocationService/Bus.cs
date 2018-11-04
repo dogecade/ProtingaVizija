79a8b5592f242ca469301bc66591b25745eb031c
Deividas <deividas.brazenas@gmail.com>
2018-11-04 23:22:59 +0200

    Adding funcionality of bus location

---
+++ SmartVision/LocationService/Bus.cs
@@ -0,0 +1,18 @@
+﻿namespace LocationService
+{
+    public class Bus
+    {
+        private string busRoute;
+        private string busNumber;
+        private double latitude;
+        private double longitude;
+
+        public Bus(string busNumber, double latitude, double longitude)
+        {
+            this.busNumber = busNumber;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            busRoute = LocationHelpers.GetRouteName(busNumber);
+        }
+    }
+}
