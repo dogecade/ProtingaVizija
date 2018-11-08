8fe75815b23cf5f2f6c0c8b469e243c3633028f1
Arnas Danaitis <arnas.danaitis@gmail.com>
2018-11-08 08:38:43 +0200

    Added basic option for persistent processing

--- SmartVision/FaceAnalysis/VideoSourceHandler.cs
+++ SmartVision/FaceAnalysis/VideoSourceHandler.cs
@@ -8,7 +8,13 @@ namespace FaceAnalysis
 {
     public class VideoSourceHandler
     {
-        public static List<ProcessableVideoSource> Sources { get; } = new List<ProcessableVideoSource>();
+        private static FaceProcessor processor;
+        public static void EstablishProcessor()
+        {
+            processor?.Complete();
+            processor = new FaceProcessor(Sources);
+        }
+        public static List<ProcessableVideoSource> Sources { get; set; } = new List<ProcessableVideoSource>();
         public static void AddSource(ProcessableVideoSource source) => Sources.Add(source);
         public static void RemoveSource(ProcessableVideoSource source) => Sources.Remove(source);
     }
