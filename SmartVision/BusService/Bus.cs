e269618705884e947f7cc2dbf30e098b26ea0d48
Deividas Brazenas <deividas.brazenas@gmail.com>
2018-11-13 18:57:07 +0200

    Starting improvements

--- SmartVision/BusService/Bus.cs
+++ SmartVision/BusService/Bus.cs
@@ -5,14 +5,23 @@
         public BusType busType { get; }
         public string busRoute { get; }
         public string busNumber { get; }
+        public long routeId { get; }
+        public int busId { get; }
         public double latitude { get; }
         public double longitude { get; }
         public int speed { get; }
 
-        public Bus(int busType, string busNumber, double latitude, double longitude, int speed)
+        public Bus(string busType, string busNumber, long routeId, int busId, double latitude, double longitude, int speed)
         {
-            this.busType = (BusType)busType;
+            if (busType == "Autobusai")
+                this.busType = BusType.Bus;
+
+            else
+                this.busType = BusType.Trolley;
+
             this.busNumber = busNumber;
+            this.routeId = routeId;
+            this.busId = busId;
             this.latitude = latitude;
             this.longitude = longitude;
             this.speed = speed;
