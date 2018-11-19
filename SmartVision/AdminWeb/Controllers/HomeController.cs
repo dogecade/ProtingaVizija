1597358a7e411616daaacb874eed6442c8602573
tomas-drasutis <drasutis.tomas@gmail.com>
2018-11-19 21:25:14 +0200

    Multiple streams are loaded into the website, after addition it appears in the page as well.

--- SmartVision/AdminWeb/Controllers/HomeController.cs
+++ SmartVision/AdminWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,26 @@ namespace AdminWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<string> URLlist = new List<string>{ "https://webcam1.lpl.org/axis-cgi/mjpg/video.cgi" , "http://quadcam.unrfound.unr.edu/axis-cgi/mjpg/video.cgi" };
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddCamera(String streamUrl)
+        {
+            Debug.WriteLine(streamUrl);
+            if (!streamUrl.Equals(""))
+            {
+                streamUrl = streamUrl.Replace(@"""", string.Empty);
+                URLlist.Add(streamUrl);
+            }
+            return Json(new { result = "succ" }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult GetStreamList()
+        {
+            return Json(new { result = URLlist }, JsonRequestBehavior.AllowGet);
         }
 
     }
