a0a090e6fc779d67ac526f6b58a48378a60f2997
dogecade <32038350+dogecade@users.noreply.github.com>
2018-11-21 12:50:56 +0200

    Update Keys.cs

--- SmartVision/Constants/Keys.cs
+++ SmartVision/Constants/Keys.cs
@@ -2,8 +2,8 @@
 {
     public class Keys
     {
-        public const string faceApiKey = "dAlu9ya0aHu7EvNceurYv6Dq1cMyPAtT";
-        public const string faceApiSecret = "4HKjVcWDBg9V2g8i0MqCZaM-VD77elhT";
+        public const string faceApiKey = "";
+        public const string faceApiSecret = "";
         public const string facesetToken = "";
 
         public const string twilioAccountSid = "";
