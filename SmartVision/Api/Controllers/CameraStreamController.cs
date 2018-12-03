aca83b242d1ce4f3c1b83bc24c67d2a29c3851d0
Deividas <deividas.brazenas@gmail.com>
2018-12-03 21:08:39 +0200

    Adding photo upload feature. Tomas will edit the UI

--- SmartVision/Api/Controllers/CameraStreamController.cs
+++ SmartVision/Api/Controllers/CameraStreamController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Constants;
+using FaceAnalysis;
+using Helpers;
 
 namespace Api.Controllers
 {
@@ -22,19 +25,60 @@ namespace Api.Controllers
         }
 
         [HttpPost]
-        public void CaptureSnapshot(string imgBase64)
+        //this should probably return action result, otherwise nothing will happen in the frontend IIRC
+        public async Task<ActionResult> CaptureSnapshot(string imgBase64)
         {
-            imgBase64 = imgBase64.Replace(" ", "+");
-            Byte[] bitmapData = Convert.FromBase64String(FixBase64ForImage(imgBase64));
-            System.IO.MemoryStream streamBitmap = new System.IO.MemoryStream(bitmapData);
-            Bitmap bitImage = new Bitmap((Bitmap)Image.FromStream(streamBitmap));
+            FaceApiCalls apiCalls = new FaceApiCalls(new HttpClientWrapper());
 
-            bitImage.Save(@"C:\dev\foto.png");
+            // Prepare base64 string
+            imgBase64 = imgBase64.Substring(imgBase64.IndexOf("base64,", StringComparison.Ordinal) + 7);
+            imgBase64 = imgBase64.Substring(0, imgBase64.LastIndexOf("\"", StringComparison.Ordinal));
+            imgBase64 = imgBase64.Replace(" ", "+");
+
+            // Create a bitmap
+            byte[] bitmapData = Convert.FromBase64String(FixBase64ForImage(imgBase64));
+            System.IO.MemoryStream streamBitmap = new System.IO.MemoryStream(bitmapData);
+            Bitmap bitmap = new Bitmap((Bitmap)Image.FromStream(streamBitmap));
+
+            bitmap = HelperMethods.ProcessImage(bitmap);
+            // Analyze bitmap
+            FrameAnalysisJSON analysisResult = await FaceProcessor.ProcessFrame(bitmap);
+            if (analysisResult == null)
+            {
+                //error must've occured, should alert user.
+                return null;
+            }
+
+            if (analysisResult.Faces.Count == 0)
+            {
+                return Json(new { result = "No faces have been found in the provided picture" }, JsonRequestBehavior.AllowGet);
+            }
+
+            var biggestConfidence = LikelinessConfidence.LowProbability;
+
+            foreach (var face in analysisResult.Faces)
+            {
+                var searchResult = await apiCalls.SearchFaceInFaceset(Keys.facesetToken, face.Face_token);
+                if (searchResult != null)
+                {
+                    foreach (var likelinessResult in searchResult.LikelinessConfidences()) //might want to set the camera properties to some value.
+                    {
+                        biggestConfidence = (likelinessResult.Confidence > biggestConfidence)
+                            ? likelinessResult.Confidence
+                            : biggestConfidence;
+
+                        await SearchResultHandler.HandleOneResult(likelinessResult, LikelinessConfidence.HighProbability, cameraProperties: null);
+                    }
+                }
+
+            }
+
+            return Json(new { result = biggestConfidence.ToString() }, JsonRequestBehavior.AllowGet);
         }
         public string FixBase64ForImage(string Image)
         {
             System.Text.StringBuilder sbText = new System.Text.StringBuilder(Image, Image.Length);
-            sbText.Replace("\r\n", String.Empty); sbText.Replace(" ", String.Empty);
+            sbText.Replace("\r\n", string.Empty); sbText.Replace(" ", string.Empty);
             return sbText.ToString();
         }
     }
