f9d2c0dcf1664d034eef305947d6e5924c996b51
Tomas <vilcinskastomas911@gmail.com>
2018-12-03 19:16:33 +0200

    Revert "Revert "file work""
    This reverts commit d854ce20378eea31f420f5c81a78313cb8926490.

--- SmartVision/Api/Controllers/ImageUploadController.cs
+++ SmartVision/Api/Controllers/ImageUploadController.cs
@@ -24,7 +24,7 @@ namespace Api.Controllers
                 throw new System.Web.Http.HttpResponseException(System.Net.HttpStatusCode.UnsupportedMediaType);
             }
             string root = System.Web.HttpContext.Current.Server.MapPath("~/img");
-            var provider = new MultipartFormDataStreamProvider(root);
+            var provider = new MyMultipartFormDataStreamProvider(root);
             JavaScriptSerializer serializer = new JavaScriptSerializer();
             List<string> strings = new List<string>();
             try
@@ -32,7 +32,7 @@ namespace Api.Controllers
                 await request.Content.ReadAsMultipartAsync(provider);
                 foreach (MultipartFileData file in provider.FileData)
                 {
-                    strings.Add(file.LocalFileName);
+                    strings.Add(file.LocalFileName.Substring(file.LocalFileName.LastIndexOf('\\')));
                 }
 
                 return Ok(serializer.Serialize(strings));
@@ -43,4 +43,18 @@ namespace Api.Controllers
             }
         }
     }
+    public class MyMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public MyMultipartFormDataStreamProvider(string path) : base(path)
+        { }
+
+        public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+        {
+            // override the filename which is stored by the provider (by default is bodypart_x)
+            string originalFileName = headers.ContentDisposition.FileName.Trim('\"');
+
+            return originalFileName;
+        }
+    }
+
 }
