a6f4d76c19015411c2507d9d756d0aaa95c641c9
tomas-drasutis <drasutis.tomas@gmail.com>
2018-12-15 19:57:17 +0200

    Fixed nuggets and improved currently missing people page.

--- SmartVision/Constants/Keys.cs
+++ SmartVision/Constants/Keys.cs
@@ -4,13 +4,13 @@
     {
         public const string faceApiKey = "";
         public const string faceApiSecret = "";
-        public const string facesetToken = "43137474f7c2e1fcf637ee10e98f659c";
+        public const string facesetToken = "";
 
-        public const string twilioAccountSid = "AC1e8895bbdfc2f7752de17d658f777da9";
-        public const string twilioAuthToken = "3c96253878a3c13a1dad2876149dfe2c";
+        public const string twilioAccountSid = "";
+        public const string twilioAuthToken = "";
 
-        public const string googleApiKey = "AIzaSyB3kYjUClnhOjdxw5tW4l2CBXDu4DffNIU";
-        public const string recaptchaSecretKey = "6LfclX4UAAAAAIQY4c6iV30h39hNZxK-A4CLdQjU";
+        public const string googleApiKey = "";
+        public const string recaptchaSecretKey = "";
 
     }
 }
